b2c9bbd201f26032206bc86b4f1ed2c18a122981
Sam <sam.maddox1@yahoo.com>
2016-02-13 11:43:27 -0500

    Added if to Channels.h

--- src/Channels.h
+++ src/Channels.h
@@ -1,3 +1,6 @@
+#ifndef SRC_CHANNELS_H_
+#define SRC_CHANNELS_H_
+
 /*---------------------------------------------------
  *                      Channels
  * --------------------------------------------------
@@ -30,9 +33,9 @@ const static int CH_HANG_FW = 2;
 const static int CH_HANG_RV = 3;
 
 //--------Things not added yet TODO Set Channels---------
-const static int CH_SHOOTSTICK_PCM = CH_PCMB;
-const static int CH_SHOOTSTICK_FW = 0;
-const static int CH_SHOOTSTICK_RV = 1;
+const static int CH_SHOOTSTICK_PCM = CH_PCMA;
+const static int CH_SHOOTSTICK_FW = 6;
+const static int CH_SHOOTSTICK_RV = 7;
 const static int CH_ANGLEMOTOR = 2;
 const static int CH_LIFTMOTOR = 3;
 const static int CH_SHOOTMOTOR = 4;
@@ -59,3 +62,6 @@ const static int BUT_SUCKIN = 2;
 //TODO Is angle on buttons or joystick?
 const static int BUT_AUTOAIMA = 4;
 const static int BUT_AUTOAIMB = 5;
+
+
+#endif
